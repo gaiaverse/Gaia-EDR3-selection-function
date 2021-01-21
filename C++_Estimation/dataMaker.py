7b58ab6203adf0afe4f7440d03d1cc13de75d483
almightyjack <jackthomasfraser@gmail.com>
2021-01-21 17:58:54 +0000

    New large mock trial started + full 4d parser implemented

--- C++_Estimation/dataMaker.py
+++ C++_Estimation/dataMaker.py
@@ -6,12 +6,12 @@ a = 3
 b = 2
 c = -10
 d = 7500
-sigmaFrac = 1
+sigmaFrac = 100
 
-nFile = 10
-nDataPerFile = 500000
+nFile = 20
+nDataPerFile = 800000000
 
-box = 10000
+box = 100000
 xRange = (-box,box)
 yRange = (-box,box)
 wRange = (-box,box)
@@ -24,7 +24,7 @@ for fileID in range(0,nFile):
         f = open(name,'w')
                 
         prevFrac = 0
-        fracGap = 5
+        fracGap = 0.5
         for n in range(0,nDataPerFile):
                 
                 x = random.uniform(xRange[0],xRange[1])
@@ -38,10 +38,10 @@ for fileID in range(0,nFile):
                 line = "%.3f, %.3f, %.3f, %.3f, %.3f\n" % (x,y,w,zMeasure,sigma)
                 f.write(line)
                 
-                progress = round(100*float(n)/nDataPerFile)
+                progress = round(100*float(n)/nDataPerFile,1)
                 if progress >= prevFrac + fracGap:
                     prevFrac = progress
-                    print("\t%d %%" % progress)
+                    print("\t%.1f %%" % progress)
         f.close()
 
         
