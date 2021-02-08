bb28401f3a35f24daa19b7e9fafbd6d5dcd907de
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2021-02-08 16:42:49 +0000

    reduced size a bit more

--- C++/src/GlobalVariables.h
+++ C++/src/GlobalVariables.h
@@ -6,7 +6,7 @@ using Eigen::VectorXd;
 
 const int Nh = 2; // number of hyper-hyper-parameters
 const int Ng = 81;//35; // number of magnitude bins
-const int Nt = 896769; // number of time bins
+const int Nt = 896769	; // number of time bins
 const double SingularityPreventer = 10e-13;
 
 const double mu_mean = -3.0;
