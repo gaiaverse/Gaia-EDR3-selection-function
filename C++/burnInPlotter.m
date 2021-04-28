d887b7ef2dc0febc2639cb47678c2f6786eb2240
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2021-04-28 17:42:18 +0100

    Burn in testing continues

--- C++/burnInPlotter.m
+++ C++/burnInPlotter.m
@@ -1,20 +1,22 @@
-system('scp "tplxdt071.nat.physics.ox.ac.uk:Documents/Work/GaiaSelectionFunction/Code/C++/burnSearcher*" ../C++/');
-
-files = "burnSearcher_long" + ["1","2","3","4"]+ ".txt";
+colormap(winter);
+system('scp "tplxdt071.nat.physics.ox.ac.uk:Documents/Work/GaiaSelectionFunction/Code/C++/burnTest*" ../C++/');
+files = "burnTesting_3.txt";
 
 dotSize = 20;
 
-global x y z t success nRuns cx cy cz;
+global x y z t success nRuns cx cy cz burn tx cutoff;
 x = [];
 y = [];
 z = [];
 t = [];
+tx = [];
 cx = [];
 cy =[];
+burn = 0;
 cz = [];
 success = false(0);
 nRuns = 0;
-
+cutoff = 1000;
 
 clf;
 for file = files
@@ -35,8 +37,8 @@ end
 if length(success) < length(x)
 	success(end+1) = 0;
 end
-T = tiledlayout(1,2);
-title(T,"Base Model","FontSize",25);
+T = tiledlayout(2,2);
+% title(T,"Base Model","FontSize",25);
 
 % nexttile(T);
 % 
@@ -48,14 +50,43 @@ title(T,"Base Model","FontSize",25);
 
 
 
-
 nexttile(T);
 edges = [0:10:120];
-histogram(t,20);
-title("Calculation duration (mean = " + num2str(mean(t(~isnan(t)))) + ")");
-xlabel("Seconds");
-ylabel("Counts");
+t = t';
 
+r = [];
+b = [];
+m = [];
+for i = 1:width(t)
+    p = t(:,i);
+    b = [b;ones(height(t),1)*i];
+    r =[r;p];
+    m(i) = mean(p(~isnan(p) & p > 0));
+ end
+histogram2(r,b,40)
+ view(-12,45);
+
+
+title("Calculation duration (mean = " + mean(m) + ")");
+xlabel("Seconds");
+ylabel("Burn in");
+zlabel("Frequency");
+
+nexttile(T);
+
+plot([1:length(m)],m);
+hold on;
+plot([1:length(old_m)],old_m);
+hold off;
+xlabel("Burn in width");
+ylabel("Mean duration");
+legend(["Iterative-n Burn ins", "Block-n Burn ins"]);
+
+nexttile(T);
+surf(t);
+xlabel("burn in value");
+ylabel("random seed - 2770");
+zlabel("Duration");
 % 
 % nexttile(T);
 % 
@@ -67,11 +98,12 @@ ylabel("Counts");
 
 
 
-
+%subplot(1,2,2)
 nexttile(T);
-s = (cz >0.1);
-q = t(success);
-scatter3(cx(s),cy(s),cz(s),dotSize,q(s));
+s = (~isnan(t(:)'));
+s = s(1:length(cx));
+q = t(success(:))';
+scatter3(cx(s),cy(s),cz(s),dotSize,tx(s));
 xlabel("Nt_1"); ylabel("Nt_2"); zlabel("Ns");
 title("Duration By Final Position");
 view(45,45);
@@ -79,27 +111,32 @@ colorbar;
 
 
 function checkPos(r)
-global x y z success nRuns t;
+global x y z success nRuns t burn;
 	if contains(r, "BEGINNING")
+        burn = burn + 1;
+        nRuns = 0;
+    end
+    if contains(r,"Position Vector initialised");
 		nRuns = nRuns + 1;
-% 		[j,k] = regexp(r,"-?\d*\.{0,1}\d+");
-% 		
-% 		pos = [0,0,0];
-% 		for i = 1:length(j)
-% 			w = extractBetween(r,j(i),k(i));
-% 			pos(i) = str2double(w);
-% 		end
-% 		x(end+1) = pos(1);y(end+1)=pos(2);z(end+1) = pos(3);
+		[j,k] = regexp(r,"-?\d*\.{0,1}\d+");
 		
-        x(nRuns) = 0; y(nRuns) = 0; z(nRuns)=0;
+		pos = [0,0,0];
+		for i = 1:length(j)
+			w = extractBetween(r,j(i),k(i));
+			pos(i) = str2double(w);
+		end
+		x(burn,nRuns) = pos(1);y(burn,nRuns)=pos(2);z(burn,nRuns) = pos(3);
+		
+%         x(burn,nRuns) = 0; y(burn,nRuns) = 0; z(burn,nRuns)=0;
 
-		success(nRuns) = false;
-		t(nRuns) = NaN;
+		success(burn,nRuns) = false;
+		t(burn,nRuns) = 0;
+        
 	end
 end
 
 function checkDuration(r)
-	global success t nRuns;
+	global success t nRuns burn tx cutoff;
 
 	if contains(r,"Duration")
 		[j,k] = regexp(r,"\d+");
@@ -114,11 +151,14 @@ function checkDuration(r)
 		if contains(lower(r),"second")
             secs =  str2double(extractBetween(r,j(selector),k(selector)));
         end
-        t(nRuns) = mins*60 + secs;
-% 		if t(nRuns) > 60
-% 			t(nRuns) = NaN;
-% 		end
-		success(nRuns) = true;
+        t(burn, nRuns) = mins*60 + secs;
+        
+       
+		if t(burn,nRuns) > cutoff
+			t(burn,nRuns) = NaN;
+        end
+        tx(end+1) = t(burn,nRuns);
+		success(burn,nRuns) = true;
 	end	
 end
 
@@ -134,7 +174,7 @@ function checkConverge(r)
 			pos(i) = str2double(w);
 		end
 		cx(end+1) = pos(1);cy(end+1)=pos(2);cz(end+1) = pos(3);
-		
+
 
 	end
 
