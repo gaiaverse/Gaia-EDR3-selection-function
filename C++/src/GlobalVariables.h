a463b722624595c002b795839a36fa3eb262251d
Jack Fraser <jacktfraser@physics.ox.ac.uk>
2021-04-29 15:45:06 +0100

    Enable spatial modes

--- C++/src/GlobalVariables.h
+++ C++/src/GlobalVariables.h
@@ -11,7 +11,7 @@ const int Nm = 1;//35; // number of magnitude bins
 const int Nt = 8196; // number of time bins, coarse, feel free to change
 const int TotalScanningTime = 8967691; // number of time bins, must be 8967691, do not change!
 const int healpix_order = 0; // order of healpix map, can be any integer >= 0
-const int needlet_order = -1; // maximum order of needlets used, can be any integ*needler >= -1
+const int needlet_order = 0; // maximum order of needlets used, can be any integ*needler >= -1
 
 const int healpix_nside = pow(2,healpix_order);
 const int Nl = 12*pow(healpix_nside,2);
@@ -25,7 +25,7 @@ const double SingularityPreventer = 1e-18;
 const double mut = 3;
 const double sigmat = 1;
 const double lm = 3;
-const double lt = 10;
+const double lt = 3;
 
 const int SaveSteps = 5;
 const int PipelineMinVisits = 5; 
