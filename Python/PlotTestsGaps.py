857ded649b3f3c9d6025580da04ec16b549bc792
DouglasBoubert <douglasboubert@gmail.com>
2021-04-29 11:16:54 +0100

    Removing dependency on plt.stairs

--- Python/PlotTestsGaps.py
+++ Python/PlotTestsGaps.py
@@ -14,7 +14,8 @@ gaps = pd.read_csv('/mnt/extraspace/GaiaSelectionFunction/TestSets/gaps/edr3_gap
 
 # Plot
 tbeg, tend = 1717.6256+(np.linspace(1666.4384902198801, 2704.3655735533684, 2) + 2455197.5 - 2457023.5 - 0.25)*4
-plt.stairs(xt,np.linspace(tbeg,tend,Nt+1),lw=0.3,color='k')
+bins = np.linspace(tbeg,tend,Nt+1)
+plt.hist(0.5*(bins[1:]+bins[:-1]),bins = bins,weights=xt,lw=0.3,color='k',histtype='step')
 for start,end in zip(gaps['tbeg'].values,gaps['tend'].values):
     plt.fill_between([start,end],y1=[-10,-10],y2=[10,10],color='lightblue')
 plt.ylim([-1.05*np.abs(np.max(xt)),+1.05*np.abs(np.max(xt))])
