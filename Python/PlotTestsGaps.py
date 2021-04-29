a3a1f60d905cf980c23c8d4be72bfd9776e81daf
DouglasBoubert <douglasboubert@gmail.com>
2021-04-29 13:32:13 +0100

    Added a probability plot

--- Python/PlotTestsGaps.py
+++ Python/PlotTestsGaps.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import special
 
 # Load in parameters
 params = pd.read_csv('/mnt/extraspace/GaiaSelectionFunction/Code/C++/Test/gaps/Optimiser_Properties.dat',skipinitialspace=True)
@@ -12,7 +13,7 @@ xt = pd.read_csv('/mnt/extraspace/GaiaSelectionFunction/Code/C++/Test/gaps/Final
 # Load in gaps
 gaps = pd.read_csv('/mnt/extraspace/GaiaSelectionFunction/TestSets/gaps/edr3_gaps.csv')
 
-# Plot
+# Plot - xt
 plt.figure(figsize=(10,5))
 tbeg, tend = 1717.6256+(np.linspace(1666.4384902198801, 2704.3655735533684, 2) + 2455197.5 - 2457023.5 - 0.25)*4
 bins = np.linspace(tbeg,tend,Nt+1)
@@ -22,4 +23,16 @@ for start,end in zip(gaps['tbeg'].values,gaps['tend'].values):
 plt.ylim([-1.05*np.abs(np.max(xt)),+1.05*np.abs(np.max(xt))])
 plt.xlabel('OBMT (revolutions)')
 plt.ylabel('xt')
-plt.savefig('/mnt/extraspace/GaiaSelectionFunction/Code/C++/Test/gaps/plot.png',dpi=300,facecolor='w',bbox_inches='tight')
+plt.savefig('/mnt/extraspace/GaiaSelectionFunction/Code/C++/Test/gaps/plot_xt.png',dpi=300,facecolor='w',bbox_inches='tight')
+
+# Plot - pt
+plt.figure(figsize=(10,5))
+tbeg, tend = 1717.6256+(np.linspace(1666.4384902198801, 2704.3655735533684, 2) + 2455197.5 - 2457023.5 - 0.25)*4
+bins = np.linspace(tbeg,tend,Nt+1)
+plt.hist(0.5*(bins[1:]+bins[:-1]),bins = bins,weights=special.expit(xt),lw=0.3,color='k',histtype='step')
+for start,end in zip(gaps['tbeg'].values,gaps['tend'].values):
+    plt.fill_between([start,end],y1=[0,0],y2=[1,1],color='lightblue')
+plt.ylim([0,1])
+plt.xlabel('OBMT (revolutions)')
+plt.ylabel('pt')
+plt.savefig('/mnt/extraspace/GaiaSelectionFunction/Code/C++/Test/gaps/plot_pt.png',dpi=300,facecolor='w',bbox_inches='tight')
