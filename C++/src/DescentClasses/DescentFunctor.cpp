89d0fedd90cb709eb8d6ab6f785fd9f07ddef284
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2021-04-30 15:36:02 +0100

    magnitude debugging

--- C++/src/DescentClasses/DescentFunctor.cpp
+++ C++/src/DescentClasses/DescentFunctor.cpp
@@ -180,12 +180,19 @@ void DescentFunctor::DistributeCalculations(const TVector &RawPosition)
 	MPI_Bcast(&TransformedPosition[0], n, MPI_DOUBLE, RunningID, MPI_COMM_WORLD);
 	L.Calculate(TransformedPosition);
 	
+	
+	
+	
+	
 	//collect values
 	double l = L.Value; //as with the workers, have to store here temporarily for a reason I don't understand. It breaks if you MPI_Reduce(&L.Value), so learn from my mistake
 	double Lsum = 0;			
 	MPI_Reduce(&l, &Lsum, 1,MPI_DOUBLE, MPI_SUM, RunningID,MPI_COMM_WORLD);
 	MPI_Reduce(&L.Gradient[0], &TransformedGradient[0], n,MPI_DOUBLE, MPI_SUM, RunningID,MPI_COMM_WORLD);
 
+	
+	
+	
 	
 	BackwardTransform();
 	L.Prior(RawPosition,&Lsum,&CurrentGradient);
--- C++/src/GlobalVariables.h
+++ C++/src/GlobalVariables.h
@@ -11,7 +11,7 @@ const int Nm = 10;//35; // number of magnitude bins
 const int Nt = 1; // number of time bins, coarse, feel free to change
 const int TotalScanningTime = 8967691; // number of time bins, must be 8967691, do not change!
 const int healpix_order = 0; // order of healpix map, can be any integer >= 0
-const int needlet_order = 0; // maximum order of needlets used, can be any integ*needler >= -1
+const int needlet_order = -1; // maximum order of needlets used, can be any integ*needler >= -1
 
 
 const double mut = 5;
