965adee4ec98659f133215db0dd1b03e7a3ba81b
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2021-05-03 10:41:06 +0100

    Changed param values for rerun

--- C++/src/GlobalVariables.h
+++ C++/src/GlobalVariables.h
@@ -8,16 +8,16 @@
 using Eigen::VectorXd;
 
 const int Nm = 213;//35; // number of magnitude bins
-const int Nt = 1e6; // number of time bins, coarse, feel free to change
+const int Nt = 1e5; // number of time bins, coarse, feel free to change
 const int TotalScanningTime = 8967691; // number of time bins, must be 8967691, do not change!
 const int healpix_order = 0; // order of healpix map, can be any integer >= 0
 const int needlet_order = -1; // maximum order of needlets used, can be any integ*needler >= -1
 
 
-const double mut = 5;
+const double mut = 0;
 const double sigmat = 5;
 const double lm = 3;
-const double lt = 10;
+const double lt = 3;
 
 const int healpix_nside = pow(2,healpix_order);
 const int Nl = 12*pow(healpix_nside,2);
