3099c497cd4559496b5c8819e68fc22600e783a3
Jack Fraser <jacktfraser@physics.ox.ac.uk>
2021-05-06 15:36:53 +0100

    Enabled spatial variation + magnitudes

--- C++/src/GlobalVariables.h
+++ C++/src/GlobalVariables.h
@@ -8,11 +8,11 @@
 #include <iomanip>
 using Eigen::VectorXd;
 
-const int Nm = 1;//35; // number of magnitude bins
+const int Nm = 213;//35; // number of magnitude bins
 const int Nt = 4152; // number of time bins, coarse, feel free to change
 const int TotalScanningTime = 8967691; // number of time bins, must be 8967691, do not change!
-const int healpix_order = 1; // order of healpix map, can be any integer >= 0
-const int needlet_order = 0; // maximum order of needlets used, can be any integ*needler >= -1
+const int healpix_order = 2; // order of healpix map, can be any integer >= 0
+const int needlet_order = 1; // maximum order of needlets used, can be any integ*needler >= -1
 
 
 const double mut = 5;
