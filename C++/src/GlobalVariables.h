2e99be9732f195d0d0e1da65c0f896bcd94c1069
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2021-05-10 14:14:00 +0100

    Beefing up spatial part for testing purposes

--- C++/src/GlobalVariables.h
+++ C++/src/GlobalVariables.h
@@ -13,8 +13,8 @@ const int RootID = 0; //<- declare that process 0 is always Root.
 const int Nm = 213;//35; // number of magnitude bins
 const int Nt = 16000; // number of time bins, coarse, feel free to change
 const int TotalScanningTime = 8967691; // number of time bins, must be 8967691, do not change!
-const int healpix_order = 3; // order of healpix map, can be any integer >= 0
-const int needlet_order = 2; // maximum order of needlets used, can be any integ*needler >= -1
+const int healpix_order = 4; // order of healpix map, can be any integer >= 0
+const int needlet_order = 3; // maximum order of needlets used, can be any integ*needler >= -1
 
 const int N_SGD_Batches = 128;
 const int DataLoadCount = 16000;	//set to a value > 0, this truncates any datafile readin to that many lines
