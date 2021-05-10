9250f2e8fa6ea020a40b5c90f3dfccee7eb6434d
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2021-05-10 17:21:02 +0100

    Matlab plotter added

---
+++ C++/Plotter.m
@@ -0,0 +1,135 @@
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
+set(0,'defaultTextInterpreter','latex');
+
+files = ["TestBase","TestVectors","TestBroadcast","TestDumb"];
+folder = "TestBase";
+getData(30)
+
+
+temporalPlot(folder);
+progressPlot(files)
+
+function getData(timeGap)
+    f = load("SyncTime.mat");
+    SyncCurrentTime = datetime('now');
+    timeSince = seconds(SyncCurrentTime - f.SyncCurrentTime);
+% timeSince = 1000;
+    if timeSince > timeGap
+        system(' rsync -ar "jackfraser@hydra.physics.ox.ac.uk:/mnt/extraspace/GaiaSelectionFunction/Code/C++/Output/" Output/');
+        save("SyncTime.mat","SyncCurrentTime");
+    end
+end
+function temporalPlot(folder)
+    gaps = readtable("Output/edr3_gaps.csv");
+    properties = readtable("Output/" + folder + "/Optimiser_Properties.dat");
+    z= readmatrix("Output/" + folder + "/TempPositions/TempPosition_TransformedParameters.dat");
+    t = 1717.6256+(linspace(1666.4384902198801, 2704.3655735533684, 2) + 2455197.5 - 2457023.5 - 0.25)*4;
+    Nt = properties.Nt(1);
+    
+    figure(1);
+    T = tiledlayout(1,1,'Padding','compact','TileSpacing','compact');
+    nexttile(T);
+    f = z(1:Nt);
+    m = z(Nt+1:end);
+    x = linspace(t(1),t(2),length(f));
+    q = 1./(1 + exp(-f));
+    z = q;
+    plot(x,z,'k');
+    hold on;
+
+    for i = 1:height(gaps)
+        t1 = floor(gaps.tbeg(i))-1;
+        t2 = ceil(gaps.tend(i))+1;
+        fill([t1,t1,t2,t2],[0,1,1,0],'b','LineStyle','None','FaceAlpha',0.3);
+    end
+
+    hold off;
+    legend("pt","Known Gaps");
+end
+function progressPlot(files)
+
+    figure(2);
+
+    patterns = ["-","-","-","--"];
+    cols = colororder;
+    cols2 = min(1,cols*1.5);
+    minLim = 1;
+    clf;
+    hold on;
+    for i = 1:length(files)
+        file = "Output/" + files(i) + "/OptimiserProgress.txt";
+        f = readtable(file);
+    
+        fullEpoch = f(f.Batch == -1,:);
+        miniBatches = f(f.Batch > -1,:);
+        subplot(2,1,1);
+        hold on;
+
+        miniX = miniBatches.Epoch-1 + miniBatches.Batch ./ miniBatches.nBatches+1e-3;
+        L0 = miniBatches.F(1);
+        xB = miniX; %miniBatches.Elapsed;
+        xF = fullEpoch.Epoch; %fullEpoch.Elapsed;
+
+%         plot(fullEpoch.Elapsed,xF);
+        plot(miniBatches.Elapsed,xB,'Color',cols2(i,:),'LineWidth',0.5,'HandleVisibility','Off');
+        
+        plot(fullEpoch.Elapsed,xF,'LineWidth',2,'Color',cols(i,:));
+        
+%         xlim([400,max(xF)])
+
+        ylabel("Complete Epochs");
+        xlabel("Time Elapsed (s)");
+        grid on;
+
+        set(gca,'yscale','log')
+        set(gca,'xscale','log')
+        
+        subplot(2,1,2);
+        hold on;
+        plot(miniBatches.Elapsed,miniBatches.F/L0,'Color',cols2(i,:),'LineWidth',0.5,'HandleVisibility','Off');
+        
+        plot(fullEpoch.Elapsed,fullEpoch.F/L0,'LineWidth',1.4,'Color',cols(i,:));
+        set(gca,'yscale','log')
+        set(gca,'xscale','log')
+          xlabel("Elapsed Time (s)");
+        ylabel("$L/L_0$");
+        hold off;
+        grid on;
+%         hold on;
+%         dL = fullEpoch.dF;
+%         dL(1) = NaN;
+%         set(gca,'yscale','log')
+% 
+% 
+%         dL = dL./fullEpoch.F;
+%         dLm = dL;
+%         dLp = dL;
+%         dLm(dLm > 0) = 1e-8;
+%         dLp(dLp < 0) = 1e-8;
+%         plot(xF,abs(dLm),'b','LineStyle',patterns(i));
+% 
+%         plot(xF,dLp,'r','LineStyle',patterns(i));
+% 
+%         legend('Negative Steps','Positive Steps');
+%         set(gca,'yscale','log');
+%         set(gca,'xscale','log')
+%         xlabel("Elapsed Time");
+%         ylabel("$\frac{\Delta L}{L}$","FontSize",15);
+%         grid on;
+
+    end
+    subplot(2,1,1);
+    legend('Baseline Model','Vectorised Improvements','Slightly reduced broadcasting',"Broadcast position individually");
+    % figure(3);
+    % cla;
+    % M = 213;
+    % for i = [1:M:height(m)-M]
+    %    msub = m(i:i+M);
+    %    psub = 1./(1 + exp(-msub));
+    %    hold on;
+    %    plot(psub);
+    %    hold off;
+    % end
+
+end
+
