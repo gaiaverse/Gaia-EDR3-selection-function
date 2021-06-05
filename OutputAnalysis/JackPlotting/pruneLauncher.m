965b164168925961d8bdbd90c511b47d8526cdbc
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2021-06-05 13:56:54 +0100

    Pruning methods added

--- OutputAnalysis/JackPlotting/pruneLauncher.m
+++ OutputAnalysis/JackPlotting/pruneLauncher.m
@@ -1,6 +1,6 @@
 matlab.bigdata.internal.executor.ProgressReporter.override(matlab.bigdata.internal.executor.NullProgressReporter);
 for i = 100:100
-target = "../../../CodeOutput/Diagnostic29_PostProcessing/PostProcessing/" + num2str(i) + ".dat";
+target = "../../../Output/Diagnostic29_PostProcessing/PostProcessing/" + num2str(i) + ".dat";
 data = "../../../Data/MainData/" + num2str(i) + ".csv";
 output = "../../../Data/PrunedData/" + num2str(i) + ".csv";
 pruneData(target,data,output,0.1)
