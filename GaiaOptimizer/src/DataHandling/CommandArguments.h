a9890196e885056fa36330294b4ee071f242d05c
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2021-06-12 14:17:10 +0100

    Streamlined DataHandling.h into header-only library

--- GaiaOptimizer/src/DataHandling/CommandArguments.h
+++ GaiaOptimizer/src/DataHandling/CommandArguments.h
@@ -21,10 +21,45 @@ class CommandArgs
 		Argument<std::string> OutputDirectory = Argument<std::string>("Output","output");
 		
 		
+		//put pointers in here so can easily loop over the (heterogenous) array
 		std::vector<JSL::ArgumentInterface *> argPointers = {&RandomSeed, &StartVectorLocation, &GradLim, &MaxSteps, &FreezeSteps, &DataSource, &OutputDirectory};
-		CommandArgs();
 		
-		void ReadArguments(int argc, char *argv[],int ProcessRank);
+		CommandArgs(){};
+		
+		void ReadArguments(int argc, char *argv[],int ProcessRank)
+		{
+			//check if using config file or command line arguments
+			Argument<std::string> ConfigFile = Argument<std::string>("__null_location__","config",argc,argv);
+			Argument<char> ConfigDelimiter = Argument<char>(' ',"config-delim",argc,argv);
+			
+			if ((std::string)ConfigFile == "__null_location__")
+			{
+				for (int i = 0; i < argPointers.size(); ++i)
+				{
+					argPointers[i]->ListParse(argc,argv);
+				}
+			}
+			else
+			{
+				for (int i = 0; i < argPointers.size(); ++i)
+				{
+					argPointers[i]->Configure(ConfigFile,ConfigDelimiter);
+				}
+			}
+			
+			
+			//if necessary, create the directories necessary for output saving
+			if (ProcessRank == RootID)
+			{
+				JSL::mkdirReturn dirReport = JSL::mkdirSafely(OutputDirectory);
+				JSL::mkdirReturn dirReport2= JSL::mkdirSafely((std::string)OutputDirectory + "/" + (std::string)TempDirName);
+				if ((dirReport.Successful || dirReport2.Successful) == false)
+				{
+					ERROR(1,"Could not locate or create the output directory " + (std::string)OutputDirectory + " or subdirectories therein.");
+				}
+			}
+	
+		}
 		
 	private:
 	
